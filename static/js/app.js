3fc8f76952816aa0823e30f5df7c9ba8a58bed42
aadityavikram <aadityavikram628@gmail.com>
2020-10-12 23:45:29 +0530

    Updated Home Page to display table

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,30 @@
-const getWeatherInfo = (location, message_1, message_2, message_3, message_4, message_5, message_6, message_7, message_8) => {
+const getWeatherInfo = (location, message_1, message_2, message_3, message_4, message_5, message_6, message_7, message_8, message_9, weatherTable) => {
     message_1.textContent = 'Loading....'
     message_2.textContent = message_3.textContent = message_4.textContent = ''
     message_5.textContent = message_6.textContent = message_7.textContent = message_8.textContent = ''
+    message_5.textContent = ''
     fetch('/weather?address=' + decodeURIComponent(location)).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             message_1.textContent = data.error
         }
         else {
-            message_1.textContent = 'Temperature is ' + data.temperature + ' degrees F'
-            message_2.textContent = 'Feels Like ' + data.feelslike + ' degrees F'
-            message_3.textContent = 'Location is ' + data.location
-            message_4.textContent = 'Timezone is ' + data.timezone
-            message_5.textContent = 'Local Time is ' + data.localTime
-            message_6.textContent = 'Latitude is ' + data.latitude + ' degrees'
-            message_7.textContent = 'Longitude is ' + data.longitude + ' degrees'
+            message_1.style.display = 'none'
+            weatherTable.style.display = 'flex'
+            // message_2.textContent = 'Temperature is ' + data.temperature + ' degrees F'
+            // message_3.textContent = 'Feels Like ' + data.feelslike + ' degrees F'
+            // message_4.textContent = 'Location is ' + data.location
+            // message_5.textContent = 'Timezone is ' + data.timezone
+            // message_6.textContent = 'Local Time is ' + data.localTime
+            // message_7.textContent = 'Latitude is ' + data.latitude + ' degrees'
+            // message_8.textContent = 'Longitude is ' + data.longitude + ' degrees'
+            message_2.textContent = data.temperature
+            message_3.textContent = data.feelslike
+            message_4.textContent = data.location
+            message_5.textContent = data.timezone
+            message_6.textContent = data.localTime
+            message_7.textContent = data.latitude
+            message_8.textContent = data.longitude
             var time = ''
             if(data.is_day == 'no') { 
                 time = 'night'
@@ -22,7 +32,8 @@ const getWeatherInfo = (location, message_1, message_2, message_3, message_4, me
             } else {
                 time = 'day'
             }
-            message_8.textContent = 'It is ' + time + ' here right now!'
+            // message_9.textContent = 'It is ' + time + ' here right now!'
+            message_9.textContent = time
         }
     })
 })
@@ -38,9 +49,11 @@ const message_5 = document.querySelector('#message_5')
 const message_6 = document.querySelector('#message_6')
 const message_7 = document.querySelector('#message_7')
 const message_8 = document.querySelector('#message_8')
+const message_9 = document.querySelector('#message_9')
+const weatherTable = document.querySelector('#weatherTable')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
-    getWeatherInfo(location, message_1, message_2, message_3, message_4, message_5, message_6, message_7, message_8)
+    getWeatherInfo(location, message_1, message_2, message_3, message_4, message_5, message_6, message_7, message_8, message_9, weatherTable)
 })
